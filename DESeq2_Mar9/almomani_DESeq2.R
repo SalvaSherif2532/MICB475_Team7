848b6a05a1c5a1b7a251a37747882a6391dcba71
nl0810 <normanli1234@gmail.com>
2025-03-11 15:39:45 -0700

    Updated the DESeq2 code

--- DESeq2_Mar9/almomani_DESeq2.R
+++ DESeq2_Mar9/almomani_DESeq2.R
@@ -17,54 +17,54 @@ load("alpha_beta_diversity_analyses_Mar2/almomani_final.RData")
 almomani_plus1 <- transform_sample_counts(almomani_final, function(x) x+1)
 almomani_deseq <- phyloseq_to_deseq2(almomani_plus1, ~`env_medium`)
 DESEQ_almomani <- DESeq(almomani_deseq)
-res <- results(DESEQ_almomani, tidy=TRUE, 
+almomani_res <- results(DESEQ_almomani, tidy=TRUE, 
                # this will ensure that No is your reference group
                contrast = c("env_medium","Sputum","Swab"))
 
 ## Look at results ##
-View(res)
+View(almomani_res)
 
 ## Volcano plot: effect size VS significance ##
-ggplot(res) +
+ggplot(almomani_res) +
   geom_point(aes(x=log2FoldChange, y=-log10(padj)))
 
 ## Make variable to color by whether it is significant + large change ##
-vol_plot <- res %>%
+almomani_vol_plot <- almomani_res %>%
   mutate(significant = padj<0.01 & abs(log2FoldChange)>2) %>%
   ggplot() +
   geom_point(aes(x=log2FoldChange, y=-log10(padj), col=significant))
-vol_plot
+almomani_vol_plot
 
 ## Save volcano plot ##
-ggsave(filename="DESeq2_Mar9/almomani_vol_plot.png",vol_plot)
+ggsave(filename="DESeq2_Mar9/almomani_vol_plot.png", almomani_vol_plot)
 
 ## Filter padj and rename 'row' to 'ASV' ##
-sigASVs <- res %>% 
+almomani_sigASVs <- almomani_res %>% 
   filter(padj<0.01 & abs(log2FoldChange)>2) %>%
   dplyr::rename(ASV=row)
-View(sigASVs)
+View(almomani_sigASVs)
 
 ## Get only ASV names ##
-sigASVs_vec <- sigASVs %>%
+almomani_sigASVs_vec <- almomani_sigASVs %>%
   pull(ASV)
 
 ## Prune phyloseq file ##
-almomani_DESeq <- prune_taxa(sigASVs_vec, almomani_final)
-sigASVs <- tax_table(almomani_DESeq) %>% as.data.frame() %>%
+almomani_DESeq <- prune_taxa(almomani_sigASVs_vec, almomani_final)
+almomani_sigASVs <- tax_table(almomani_DESeq) %>% as.data.frame() %>%
   rownames_to_column(var="ASV") %>%
-  right_join(sigASVs) %>%
+  right_join(almomani_sigASVs) %>%
   arrange(log2FoldChange) %>%
   mutate(Genus = make.unique(Genus)) %>%
   mutate(Genus = factor(Genus, levels=unique(Genus)))
 
 ## Make plot of sigASVs ##
-sigASVs_plot <- ggplot(sigASVs) +
+almomani_sigASVs_plot <- ggplot(almomani_sigASVs) +
   geom_bar(aes(x=Genus, y=log2FoldChange), stat="identity")+
   geom_errorbar(aes(x=Genus, ymin=log2FoldChange-lfcSE, ymax=log2FoldChange+lfcSE)) +
   theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5, size = 3),
         axis.text.y = element_text(size = 5),
         axis.title = element_text(size = 8))
-sigASVs_plot
+almomani_sigASVs_plot
 
 ## Save sigASVs plot ##
-ggsave(filename="DESeq2_Mar9/almomani_sigASVs_plot.png", sigASVs_plot)
+ggsave(filename="DESeq2_Mar9/almomani_sigASVs_plot.png", almomani_sigASVs_plot)
--- DESeq2_Mar9/galeeva_DESeq2.R
+++ DESeq2_Mar9/galeeva_DESeq2.R
@@ -17,54 +17,54 @@ load("alpha_beta_diversity_analyses_Mar2/galeeva_final.RData")
 galeeva_plus1 <- transform_sample_counts(galeeva_final, function(x) x+1)
 galeeva_deseq <- phyloseq_to_deseq2(galeeva_plus1, ~`inpatient`)
 DESEQ_galeeva <- DESeq(galeeva_deseq)
-res <- results(DESEQ_galeeva, tidy=TRUE, 
+galeeva_res <- results(DESEQ_galeeva, tidy=TRUE, 
                # this will ensure that No is your reference group
                contrast = c("inpatient","Hospitalized","Ambulatory treatment"))
 
 ## Look at results ##
-View(res)
+View(galeeva_res)
 
 ## Volcano plot: effect size VS significance ##
-ggplot(res) +
+ggplot(galeeva_res) +
   geom_point(aes(x=log2FoldChange, y=-log10(padj)))
 
 ## Make variable to color by whether it is significant + large change ##
-vol_plot <- res %>%
+galeeva_vol_plot <- galeeva_res %>%
   mutate(significant = padj<0.01 & abs(log2FoldChange)>2) %>%
   ggplot() +
   geom_point(aes(x=log2FoldChange, y=-log10(padj), col=significant))
-vol_plot
+galeeva_vol_plot
 
 ## Save volcano plot ##
-ggsave(filename="DESeq2_Mar9/galeeva_vol_plot.png",vol_plot)
+ggsave(filename="DESeq2_Mar9/galeeva_vol_plot.png",galeeva_vol_plot)
 
 ## Filter padj and rename 'row' to 'ASV' ##
-sigASVs <- res %>% 
+galeeva_sigASVs <- galeeva_res %>% 
   filter(padj<0.01 & abs(log2FoldChange)>2) %>%
   dplyr::rename(ASV=row)
-View(sigASVs)
+View(galeeva_sigASVs)
 
 ## Get only ASV names ##
-sigASVs_vec <- sigASVs %>%
+galeeva_sigASVs_vec <- galeeva_sigASVs %>%
   pull(ASV)
 
 ## Prune phyloseq file ##
-galeeva_DESeq <- prune_taxa(sigASVs_vec, galeeva_final)
-sigASVs <- tax_table(galeeva_DESeq) %>% as.data.frame() %>%
+galeeva_DESeq <- prune_taxa(galeeva_sigASVs_vec, galeeva_final)
+galeeva_sigASVs <- tax_table(galeeva_DESeq) %>% as.data.frame() %>%
   rownames_to_column(var="ASV") %>%
-  right_join(sigASVs) %>%
+  right_join(galeeva_sigASVs) %>%
   arrange(log2FoldChange) %>%
   mutate(Genus = make.unique(Genus)) %>%
   mutate(Genus = factor(Genus, levels=unique(Genus)))
 
 ## Make plot of sigASVs ##
-sigASVs_plot <- ggplot(sigASVs) +
+galeeva_sigASVs_plot <- ggplot(galeeva_sigASVs) +
   geom_bar(aes(x=Genus, y=log2FoldChange), stat="identity")+
   geom_errorbar(aes(x=Genus, ymin=log2FoldChange-lfcSE, ymax=log2FoldChange+lfcSE)) +
   theme(axis.text.x = element_text(angle=90, hjust=1, vjust=0.5, size = 3),
         axis.text.y = element_text(size = 5),
         axis.title = element_text(size = 8))
-sigASVs_plot
+galeeva_sigASVs_plot
 
 ## Save sigASVs plot ##
-ggsave(filename="DESeq2_Mar9/galeeva_sigASVs_plot.png", sigASVs_plot)
+ggsave(filename="DESeq2_Mar9/galeeva_sigASVs_plot.png", galeeva_sigASVs_plot)
