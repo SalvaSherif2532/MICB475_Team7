5b203b6f2d131f5b606b35cddbf4bef3c23ea7b1
SalvaSherif2532 <salvasherif@gmail.com>
2025-03-11 16:13:04 -0700

    Taxa bar plots for galeeva inpatient, almomani environment

--- taxabar_plots/taxa_bar_plots.R
+++ taxabar_plots/taxa_bar_plots.R
@@ -28,20 +28,18 @@ galeeva_RA_long <- psmelt(galeeva_aggreg_RA) %>%
   summarize(MeanAbundance = mean(Abundance, na.rm = TRUE), .groups = "drop") %>%
   filter(!is.na(MeanAbundance) & MeanAbundance > 0)
 
-gg_taxa <- ggplot(galeeva_relab_long, aes(x = inpatient, y = MeanAbundance, fill = Phylum)) +
+gg_taxa <- ggplot(galeeva_RA_long, aes(x = inpatient, y = MeanAbundance, fill = Phylum)) +
   geom_bar(stat = "identity") +
   theme_minimal() +
   ggtitle("Mean Taxonomic Composition by Inpatient Status") +
   scale_fill_viridis(discrete = TRUE)
 
-ggsave("taxonomy_galeeva.png"
-       , gg_taxa
-       , height=8, width =12)
+ggsave("taxonomy_galeeva.png" , gg_taxa, height=8, width =12)
 
 # Everything for almomani sputum vs swab
 almomani_aggreg <- merge_samples(almomani, "env_medium")
-almomani_aggreg_RA <- transform_sample_counts(galeeva_aggreg, function(x) x/sum(x))
-sample_data(almomani_aggreg_RA)$env_medium <- rownames(sample_data(galeeva_aggreg_RA))
+almomani_aggreg_RA <- transform_sample_counts(almomani_aggreg, function(x) x/sum(x))
+sample_data(almomani_aggreg_RA)$env_medium <- rownames(sample_data(almomani_aggreg))
 
 almomani_RA_long <- psmelt(almomani_aggreg_RA) %>%
   group_by(env_medium, Phylum) %>%
@@ -55,6 +53,6 @@ gg_taxa_almomani <- ggplot(almomani_RA_long, aes(x = env_medium, y = MeanAbundan
   scale_fill_viridis(discrete = TRUE)
 
 ggsave("taxonomy_almomani.png"
-       , gg_taxa
+       , gg_taxa_almomani
        , height=8, width =12)
 
